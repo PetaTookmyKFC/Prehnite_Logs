26ebba4b9910b198b27ebd008847613f1f893afc
PetaTookmyKFC <36772620+PetaTookmyKFC@users.noreply.github.com>
2024-05-26 09:53:40 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+}
+
+type Logger struct {
+	Directory string
+}
+
+// The ledger will be used to organise the loggers
+// and keep track of them. Including where they are saved,
+// and the functions to create and find them.
+type Ledger struct {
+}
